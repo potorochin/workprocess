4d60944c5ca75f4475311fd5d8ecf047ebc32bdb
Bogdan <dp251196pbv@gmail.com>
2018-08-13 18:07:50 +0300

    2) Day One, check name element

--- test.erl
+++ test.erl
@@ -1,5 +1,5 @@
 -module (test).
--export ([xmlToProp/1, findXml/2, countList/2]).
+-export ([xmlToProp/1, findXml/2, countList/2, checkName/4]).
 
 
 xmlToProp(FileName) -> 
@@ -17,7 +17,7 @@ xmlToProp(FileName) ->
 	findXml([], Acc) -> Acc;
 
 	findXml([Head | Tail], Acc) -> 
-		io:format("~p~n", [Head]),
+		%io:format("~p~n", [Head]),
 		StartPoint = binary:matches(Head, <<"<">>),
 		EndPoint = binary:matches(Head, <<">">>),
 		EndStart = binary:matches(Head, <<"</">>),
@@ -26,8 +26,10 @@ xmlToProp(FileName) ->
 		Rez2 = countList(EndPoint, 0),
 		Rez3 = countList(EndStart, 0),
 
-		if Rez1 > 0 -> 			io:format("YES");
-		true -> false		end,
+		io:format("~p~n", [checkName(StartPoint, EndPoint, Head, [])]),
+
+		%if Rez3 > 0 -> 			io:format("YES");
+		%true -> false		end,
 
 
 		findXml(Tail, Acc).
@@ -45,4 +47,11 @@ xmlToProp(FileName) ->
 % 
 %%%%%%%%%		
 
+	checkName([],[], _, Acc) -> Acc;
 
+	checkName([Head1 | Tail1], [ Head2 | Tail2], In, Acc) ->
+		{Start, _ } = Head1,
+		{End, _ } = Head2,
+		Rez =  binary:bin_to_list(In, {Start + 1,End - Start - 1}),
+		io:format("~p,~n", [Rez]),
+		checkName(Tail1, Tail2, In, [Rez | Acc]).
