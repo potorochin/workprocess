748d10da6bace25bd6e0041903e57439fae9b1ef
Bogdan <dp251196pbv@gmail.com>
2018-08-14 13:45:42 +0300

    3) Day Two, check name element and value

--- test.erl
+++ test.erl
@@ -1,15 +1,19 @@
 -module (test).
--export ([xmlToProp/1, findXml/2, countList/2, checkName/4]).
+-export ([xmlToProp/1, findXml/2, checkName/5, countList/2]).
 
 
 xmlToProp(FileName) -> 
 	io:format("~p~n", [FileName]),
 	{ok, S} = 	file:read_file(FileName),
 
+	%Rez = string:split(S, " ", all),
 	Rez = string:split(S, "\n", all),
+	%io:format("~p~n", [Rez]),
 
-	findXml(Rez, []),
-	
+
+	io:format("~p~n", [lists:reverse(findXml(Rez, []))]),
+	%%%%%%%%%%%%%% REZULTAT %%%%%%%%%%%%%%%%%%%
+	lists:reverse(findXml(Rez, [])),
 	file:close(S).
 
 	% motorcycles.xml
@@ -20,38 +24,67 @@ xmlToProp(FileName) ->
 		%io:format("~p~n", [Head]),
 		StartPoint = binary:matches(Head, <<"<">>),
 		EndPoint = binary:matches(Head, <<">">>),
-		EndStart = binary:matches(Head, <<"</">>),
-		
-		Rez1 = countList(StartPoint, 0),
-		Rez2 = countList(EndPoint, 0),
-		Rez3 = countList(EndStart, 0),
 
-		io:format("~p~n", [checkName(StartPoint, EndPoint, Head, [])]),
+		ListCheck = lists:reverse(checkName(StartPoint, EndPoint, 0, Head, [])),
 
+
+		%io:format("~p~n", [ListCheck]),
 		%if Rez3 > 0 -> 			io:format("YES");
 		%true -> false		end,
 
 
-		findXml(Tail, Acc).
+		findXml(Tail, [ListCheck | Acc]).
 
 %%%%%%%%%
-% 
-%%%%%%%%%
+% COUNTLIST
+%%%%%%%%%		
 
 	countList([], Acc) -> Acc;
 
-	countList([_ | List], Acc) ->
-		countList(List, Acc+1).
+	countList([_ | Tail], Acc) ->
+		countList(Tail, Acc + 1).
+
 
 %%%%%%%%%
-% 
-%%%%%%%%%		
+%
+%%%%%%%%%
+	checkName([],[], _, _, Acc) -> Acc;
 
-	checkName([],[], _, Acc) -> Acc;
+	checkName([Head1 | Tail1], [ Head2 | Tail2], BuffPoint, In, Acc) ->
 
-	checkName([Head1 | Tail1], [ Head2 | Tail2], In, Acc) ->
-		{Start, _ } = Head1,
-		{End, _ } = Head2,
-		Rez =  binary:bin_to_list(In, {Start + 1,End - Start - 1}),
-		io:format("~p,~n", [Rez]),
-		checkName(Tail1, Tail2, In, [Rez | Acc]).
+		XmlFind = binary:matches(In, <<"?xml">>),
+		DoctypeFind = binary:matches(In, <<"!DOCTYPE">>),
+
+		FinderXml = countList(XmlFind, 0),
+		FinderDoc = countList(DoctypeFind, 0),
+
+		case FinderXml of
+			1 -> checkName(Tail1, Tail2, 0, In, [binary:bin_to_list(In) | Acc]);
+				0 ->
+
+				case FinderDoc of
+					1 -> checkName(Tail1, Tail2, 0, In, [binary:bin_to_list(In) | Acc]);
+						0 -> 
+							{Start, _ } = Head1,
+							{End, _ } = Head2,
+
+							
+							RezKey =  binary:bin_to_list(In, {Start + 1,End - Start - 1}),
+							if BuffPoint > 0 ->
+								RezInf =  binary:bin_to_list(In, {BuffPoint, Start - BuffPoint  }),
+								checkName(Tail1, Tail2, End+1, In, [lists:append(RezInf,RezKey) | Acc]);
+								%io:format("~p~n", [RezInf]);
+								BuffPoint == 0 ->
+								checkName(Tail1, Tail2, End+1, In, [RezKey| Acc]);
+								true -> false
+							end
+
+
+
+
+
+
+					end
+		end.
+
+		
